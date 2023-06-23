fe513b4525f72666ca2b8c8643c62278e866f067
Nawaritta <Nor.benhaddou@gmail.com>
2023-06-23 02:27:51 +0000

    free stack

--- more_stack_manip.c
+++ more_stack_manip.c
@@ -14,6 +14,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || stack == NULL)
 	{
+		free_stack(stack);
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
@@ -22,6 +23,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 		printf("%c\n", (*stack)->n);
 		return;
 	}
+	free_stack(stack);
 	fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 	exit(EXIT_FAILURE);
 }
