7aca8ca877e9437453a8fccabf6a93e57f0fa7d6
Nawaritta <Nor.benhaddou@gmail.com>
2023-06-23 18:54:11 +0000

    pstr yesterday s version

--- more_stack_manip.c
+++ more_stack_manip.c
@@ -3,6 +3,7 @@
 
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
+
 /**
  * pchar - prints the char at the top of the stack, followed by a new line.
  * @stack: double pointer to the top of the stack
@@ -30,31 +31,6 @@ void pchar(stack_t **stack, unsigned int line_number)
 	fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 	exit(EXIT_FAILURE);
 }
-/**
- * pchar - prints the char at the top of the stack, followed by a new line.
- * @stack: double pointer to the top of the stack
- * @line_number:  the execution line number
- *
- *
-void pchar(stack_t **stack, unsigned int line_number)
-{
-
-	if (*stack == NULL || stack == NULL)
-	{
-		free_stack(stack);
-		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	if (is_ascii((*stack)->n))
-	{
-		fprintf(stdout, "%c\n", (*stack)->n);
-		return;
-	}
-	free_stack(stack);
-	fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
-	exit(EXIT_FAILURE);
-}
-*/
 
 /**
  * pstr -  prints the string starting at the top of the stack
@@ -65,18 +41,19 @@ void pchar(stack_t **stack, unsigned int line_number)
 void pstr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
+	char c;
 
 	(void) line_number;
 
 	if (stack != NULL && *stack != NULL)
 	{
 		tmp = *stack;
-
 		while (tmp != NULL && tmp->n != 0 && is_ascii(tmp->n))
 		{
-			fprintf(stdout, "%c", tmp->n);
+			c = (char)tmp->n;
+			write(1, &c, 1);
 			tmp = tmp->next;
 		}
 	}
-	fprintf(stdout, "\n");
+	write(1, "\n", 1);
 }
